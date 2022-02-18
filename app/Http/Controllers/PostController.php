802b30b7d0ea5ba47c8031758f983063a9c922aa
oqi7 <ririna.0917@gmail.com>
2022-02-19 01:44:20 +0900

    add 08-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return view('index')->with(['posts' => $post->get()]);
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]);  
     }
 }
+?>
