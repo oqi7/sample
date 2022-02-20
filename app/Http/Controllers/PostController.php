5bf610be05a28dbfb0a96c1b531caf77956ebc05
oqi7 <ririna.0917@gmail.com>
2022-02-20 18:10:20 +0900

    08-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Http\Requests\PostRequest;
+use App\Cotegory;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return $post->get();
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+    
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
+    }
+
 }
