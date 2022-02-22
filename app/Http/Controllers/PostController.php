9ab8ba96054f62954ae2358ad6e070a76cbf823a
oqi7 <ririna.0917@gmail.com>
2022-02-22 15:03:13 +0900

    add 08-4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Http\Requests\PostRequest;
+use App\Cotegory;
+
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-    return $post->get();
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('create');
+    }
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
